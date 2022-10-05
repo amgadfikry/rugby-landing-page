5667bf8b8b31f6a9c821943989899e18a17c181a
amgadfikry <dr.amgad_sh92@yahoo.com>
2022-10-05 02:15:49 +0200

    second changes

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ menuIcon.addEventListener("click",function(){
 closeIcon.addEventListener("click",function(){
     menu.classList.toggle("inactive")
 })
+
+//auto scrolling photos in main section
+const photos = document.querySelectorAll(".main-photos img");
+const dots = document.querySelectorAll(".dot");
+const photoLength = photos.length
+let start = 0;
+
+setInterval(function(){
+    photos.forEach(el=>{
+        el.classList.remove("active-photo");
+    })
+    dots.forEach(el=>{
+        el.classList.remove("active-dot")
+    })
+    if(start == photoLength -1){
+        start = 0;
+        photos[start].classList.add("active-photo");
+        dots[start].classList.add("active-dot");
+    }
+    else{
+        start +=1;
+        photos[start].classList.add("active-photo");
+        dots[start].classList.add("active-dot");
+    }
+}
+    ,3000)
+
